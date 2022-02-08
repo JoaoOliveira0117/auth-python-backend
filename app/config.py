8eb05306424666eff0471c0dac3096b8eeed0b24
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2022-02-07 21:32:38 -0300

    remove sqlite + add mongodb local

--- app/config.py
+++ app/config.py
@@ -1,13 +1,17 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
+
+from .db_credentials import DevelopmentConfig
 
 blueprint = Blueprint("api", __name__)
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+app.config.from_object(DevelopmentConfig)
+db = MongoEngine()
+db.init_app(app)
 
 def create_app() -> Flask:
     app.register_blueprint(blueprint)
+    
     return app
---
+++ app/db_credentials.py
@@ -0,0 +1,6 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MONGODB_SETTINGS = {
+        "db": "python",
+        "host": "localhost:27017"
+    }
