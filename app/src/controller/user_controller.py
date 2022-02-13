366b4a76937e7fefc8f50984a0309a8a176113f5
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2022-02-13 16:23:37 -0300

    small cleanup

--- app/src/controller/user_controller.py
+++ app/src/controller/user_controller.py
@@ -1,22 +1,11 @@
-import uuid
 from flask import jsonify, make_response, request
-from flask_restx import Namespace, Resource, fields
-import jwt
-from app.config import app
+from flask_restx import Namespace, Resource
 from app.src.helpers.auth import auth
 
-from app.src.model.user import UserModel
-from app.src.services.user_services import create_user, get_by_username
-from app.utils.encrypt import encrypt, validate
+from app.src.services.user_services import create_user
+from app.utils.encrypt import encrypt
 
-api = Namespace("user", description="user registration")
-
-_user = api.model("user", {
-    "uid": fields.String(description="user id"),
-    "username": fields.String(description="user username"), 
-    "name": fields.String(description="user name"),
-})
-
+api = Namespace("user", description="User registration and login")
 
 @api.route("/register")
 class User(Resource):
@@ -41,10 +30,5 @@ class Login(Resource):
             "username": request.json["username"],
             "password": request.json["password"],
         }
-
-        token = request.headers["Authorization"]
-        test = jwt.decode(token, app.config['SECRET_KEY'],algorithms="HS256")
-
-        print(test)
 
         return auth(credentials["username"], credentials["password"])
